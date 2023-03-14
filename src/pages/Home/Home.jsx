9fd0a385abe8c23491f58b2ab451431e38b568da
nuheajiohc <jaehun341012@gmail.com>
2023-03-14 23:10:28 +0900

    refactor: 어플 이름 컴포넌트화

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Title from "../../components/Title/Title";
 import classes from "./Home.module.css";
 
 const Home = () => {
   return (
-    <section className={classes["home-section"]}>
-      <header className={classes.title}>나만의 민팅</header>
+    <section>
+      <Title></Title>
       <main className={classes.content}>
         성격 유형 검사하고 <br />
         <br />
