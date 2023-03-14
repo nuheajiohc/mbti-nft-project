fb6c4e25e958c5beb5f5f218761264e73bf16f30
nuheajiohc <jaehun341012@gmail.com>
2023-03-14 23:10:53 +0900

    feat: 성격 유형 검사 하는 페이지 완료

--- src/pages/PersonalityTest/PersonalityTest.jsx
+++ src/pages/PersonalityTest/PersonalityTest.jsx
@@ -1,7 +1,29 @@
+import Title from "../../components/Title/Title";
 import classes from "./PersonalityTest.module.css";
 
 const PersonalityTest = () => {
-  return <div>성격 검사 페이지</div>;
+  return (
+    <section>
+      <Title></Title>
+      <div className={classes.progress}>
+        <div className={classes["progress-done"]}>1 / 12</div>
+      </div>
+      <div className={classes["question-container"]}>
+        <div className={classes["question-number"]}>Q 1.</div>
+        <div className={classes.question}>
+          제가 프론트엔드 개발자로 성공할 수 있을까요?
+        </div>
+      </div>
+      <div className={classes["answer-container"]}>
+        <button className={classes["first-answer"]}>
+          물론입니다 대성공할 듯
+        </button>
+        <button className={classes["second-answer"]}>
+          당연하죠 일류가 될겁니다.
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default PersonalityTest;
