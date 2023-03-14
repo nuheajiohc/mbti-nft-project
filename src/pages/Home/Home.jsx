ccc0a059ed6cefb6c8d2df5b64718d7b663b846a
nuheajiohc <jaehun341012@gmail.com>
2023-03-14 22:13:37 +0900

    feat: home 페이지 완료

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,19 @@
 import classes from "./Home.module.css";
 
 const Home = () => {
-  return <div>홈페이지</div>;
+  return (
+    <section className={classes["home-section"]}>
+      <header className={classes.title}>나만의 민팅</header>
+      <main className={classes.content}>
+        성격 유형 검사하고 <br />
+        <br />
+        나만의 NFT 발급받자!
+      </main>
+      <div>
+        <button className={classes.button}> 성격 유형 검사하러 go!</button>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
