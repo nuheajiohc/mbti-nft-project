9d3e6332da5f5729c2eabec6dab70f8661b9684e
nuheajiohc <jaehun341012@gmail.com>
2023-03-14 14:17:17 +0900

    refactor: 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import classes from "./App.module.css";
-import Router from "./components/Router/Router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Result from "./pages/Result/Result";
+import PersonalityTest from "./pages/PersonalityTest/PersonalityTest";
 
 function App() {
-  return <Router />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/personalityTest" element={<PersonalityTest />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
