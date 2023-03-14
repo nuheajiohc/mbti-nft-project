749f76b03192543ca91fa989ce5afea0218c0811
nuheajiohc <jaehun341012@gmail.com>
2023-03-14 15:15:06 +0900

    feat: 사용할 폰트 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import classes from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Reset } from "styled-reset";
+import classes from "./App.module.css";
 import Home from "./pages/Home/Home";
 import Result from "./pages/Result/Result";
 import PersonalityTest from "./pages/PersonalityTest/PersonalityTest";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/personalityTest" element={<PersonalityTest />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </Router>
+    <div className={classes.App}>
+      <Reset />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/personalityTest" element={<PersonalityTest />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
