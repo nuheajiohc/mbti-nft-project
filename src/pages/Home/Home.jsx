cf8842edce54d66b68f6a153065c36e406823557
nuheajiohc <jaehun341012@gmail.com>
2023-03-15 13:52:58 +0900

    feat: 잠깐 확인좀 부탁할게요

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import Title from "../../components/Title/Title";
 import classes from "./Home.module.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const moveToTestPage = () => {
+    navigate("/personalityTest", { replace: true });
+  };
   return (
     <section>
       <Title></Title>
@@ -11,7 +17,9 @@ const Home = () => {
         나만의 NFT 발급받자!
       </main>
       <div>
-        <button className={classes.button}> 성격 유형 검사하러 go!</button>
+        <button className={classes.button} onClick={moveToTestPage}>
+          성격 유형 검사하러 go!
+        </button>
       </div>
     </section>
   );
