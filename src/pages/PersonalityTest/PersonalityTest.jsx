aa42d1073cc7fb59fea214921fd6d45098e3ee63
nuheajiohc <jaehun341012@gmail.com>
2023-03-16 10:40:09 +0900

    feat: 테스트 페이지 완료

--- src/pages/PersonalityTest/PersonalityTest.jsx
+++ src/pages/PersonalityTest/PersonalityTest.jsx
@@ -1,25 +1,81 @@
 import Title from "../../components/Title/Title";
 import classes from "./PersonalityTest.module.css";
+import { QandAList } from "../../utils/QandAList";
+import { useState } from "react";
 
-const PersonalityTest = () => {
+const PersonalityTest = props => {
+  const [currentQandA, setCurrentQandA] = useState(QandAList[0]);
+  const [typeCount, setTypeCount] = useState({
+    E: 0,
+    I: 0,
+    N: 0,
+    S: 0,
+    F: 0,
+    T: 0,
+    P: 0,
+    J: 0,
+  });
+
+  const personalType = (selectedType, questionNumber) => {
+    const includedLastSeletedTypeCount = {
+      ...typeCount,
+      [selectedType]: typeCount[selectedType] + 1,
+    };
+    const typeCountEntries = Object.entries(includedLastSeletedTypeCount);
+    const sortedTypeCount = typeCountEntries.sort((a, b) => b[1] - a[1]);
+    let result = sortedTypeCount
+      .slice(0, 4)
+      .map(([key, value]) => key)
+      .join("");
+    return result;
+  };
+
+  const moveNextQuestion = event => {
+    const selectedAnswer = event.target.dataset.answer;
+    const questionNumber = currentQandA.number - 1;
+    const selectedType = QandAList[questionNumber][selectedAnswer].type;
+    setTypeCount({ ...typeCount, [selectedType]: typeCount[selectedType] + 1 });
+    setCurrentQandA(QandAList[questionNumber + 1]);
+
+    if (questionNumber + 1 > 11) {
+      props.onClick();
+      props.onSendTypeResult(personalType(selectedType, questionNumber + 1));
+      return;
+    }
+  };
+
   return (
     <section>
       <Title></Title>
+      <div
+        className={classes["progress-rate"]}
+      >{`${currentQandA.number} / 12`}</div>
       <div className={classes.progress}>
-        <div className={classes["progress-done"]}>1 / 12</div>
+        <div
+          className={classes["progress-done"]}
+          style={{ width: `${(300 * currentQandA.number) / 12}px` }}
+        ></div>
       </div>
       <div className={classes["question-container"]}>
-        <div className={classes["question-number"]}>Q 1.</div>
-        <div className={classes.question}>
-          제가 프론트엔드 개발자로 성공할 수 있을까요?
+        <div className={classes["question-number"]}>
+          Q {currentQandA.number}.
         </div>
+        <div className={classes.question}>{currentQandA.question}</div>
       </div>
       <div className={classes["answer-container"]}>
-        <button className={classes["first-answer"]}>
-          물론입니다 대성공할 듯
+        <button
+          className={classes["first-answer"]}
+          data-answer="firstAnswer"
+          onClick={moveNextQuestion}
+        >
+          {currentQandA.firstAnswer.content}
         </button>
-        <button className={classes["second-answer"]}>
-          당연하죠 일류가 될겁니다.
+        <button
+          className={classes["second-answer"]}
+          data-answer="secondAnswer"
+          onClick={moveNextQuestion}
+        >
+          {currentQandA.secondAnswer.content}
         </button>
       </div>
     </section>
